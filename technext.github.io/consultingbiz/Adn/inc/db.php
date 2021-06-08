249c405d30cdfa66ad2371bae626f89e58d89e77
Pauline <pauline.frare-pradal@laposte.net>
2021-06-08 11:44:42 +0200

    update for server web

--- technext.github.io/consultingbiz/Adn/inc/db.php
+++ technext.github.io/consultingbiz/Adn/inc/db.php
@@ -1,5 +1,5 @@
 <?php
 
-$pdo = new PDO('mysql:host=mysql;dbname=paulinef561_GeekGarage;host=127.0.0.1', 'root', '',[
+$pdo = new PDO('mysql:host=mysql;dbname=dbs2267479;host=db5002834948.hosting-data.io', 'dbu1895426', '.Toortoor',[
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
